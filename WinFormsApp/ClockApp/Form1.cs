7b189876acbe0ff5c211253ba8239c149d728cf0
ailurusfulg <jjm95568216@gmail.com>
2020-06-15 17:47:08 +0900

    폼 로딩 / 클로징 기입

--- WinFormsApp/ClockApp/Form1.cs
+++ WinFormsApp/ClockApp/Form1.cs
@@ -22,5 +22,25 @@ namespace ClockApp
             DateTime now = DateTime.Now;
             label1.Text = now.ToString("HH:mm:ss");
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form 로드 시 발생"); // 초기화 할 것들은 폼 로드에 기입
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Form 클로즈 시 발생");
+        }
+
+        private void Form1_Activated(object sender, EventArgs e) // 폼 클로징은 닫기 버튼 눌렀을 때, 폼 클로즈드는 아예 꺼지고 나서
+        {
+            MessageBox.Show("Form 활성화 시 발생");
+        }
+
+        private void ㄴ(object sender, EventArgs e)
+        {
+
+        }
     }
 }
