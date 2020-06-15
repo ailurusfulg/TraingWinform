d3b1bc2b95b289bdee00368c902cf6ffb975a0f8
ailurusfulg <jjm95568216@gmail.com>
2020-06-15 16:50:37 +0900

    WinFrom 현재시간

---
+++ WinFormsApp/WinFormsApp/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WinFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTNMessage = new System.Windows.Forms.Button();
+            this.TxtCurrentDate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BTNMessage
+            // 
+            this.BTNMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BTNMessage.Font = new System.Drawing.Font("나눔고딕코딩", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BTNMessage.Location = new System.Drawing.Point(644, 416);
+            this.BTNMessage.Name = "BTNMessage";
+            this.BTNMessage.Size = new System.Drawing.Size(186, 45);
+            this.BTNMessage.TabIndex = 0;
+            this.BTNMessage.Text = "현재시간";
+            this.BTNMessage.UseVisualStyleBackColor = false;
+            this.BTNMessage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtCurrentDate
+            // 
+            this.TxtCurrentDate.Location = new System.Drawing.Point(166, 42);
+            this.TxtCurrentDate.Name = "TxtCurrentDate";
+            this.TxtCurrentDate.Size = new System.Drawing.Size(393, 25);
+            this.TxtCurrentDate.TabIndex = 1;
+            this.TxtCurrentDate.TextChanged += new System.EventHandler(this.TxtCurrentDate_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(30, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "현재시간 : ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 473);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtCurrentDate);
+            this.Controls.Add(this.BTNMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTNMessage;
+        private System.Windows.Forms.TextBox TxtCurrentDate;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinFormsApp/WinFormsApp/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            // MessageBox.Show($"Hell World! {now}");
+            TxtCurrentDate.Text = now.ToString();
+        }
+
+        private void g_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtCurrentDate_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
