436c6170d930da405819ca9fd257c4d6fb0b232d
ailurusfulg <jjm95568216@gmail.com>
2020-06-17 16:13:31 +0900

    Node 추가 버튼 추가

--- WinFormsApp/WindowsFormsApp1/Form1.Designer.cs
+++ WinFormsApp/WindowsFormsApp1/Form1.Designer.cs
@@ -29,33 +29,34 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("베토벤", 2, 2);
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("모차르트", 2, 2);
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("차이코프스키", 2, 2);
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("클래식", 1, 0, new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2,
-            treeNode3});
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("노드7", 2, 2);
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("노드8", 2, 2);
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("노드9", 2, 2);
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("팝", 1, 0, new System.Windows.Forms.TreeNode[] {
-            treeNode5,
-            treeNode6,
-            treeNode7});
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("노드10", 2, 2);
-            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("노드11", 2, 2);
-            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("노드12", 2, 2);
-            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("K-pop", 1, 0, new System.Windows.Forms.TreeNode[] {
-            treeNode9,
-            treeNode10,
-            treeNode11});
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("베토벤", 2, 2);
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("모차르트", 2, 2);
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("차이코프스키", 2, 2);
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("클래식", 1, 0, new System.Windows.Forms.TreeNode[] {
+            treeNode13,
+            treeNode14,
+            treeNode15});
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("노드7", 2, 2);
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("노드8", 2, 2);
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("노드9", 2, 2);
+            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("팝", 1, 0, new System.Windows.Forms.TreeNode[] {
+            treeNode17,
+            treeNode18,
+            treeNode19});
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("노드10", 2, 2);
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("노드11", 2, 2);
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("노드12", 2, 2);
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("K-pop", 1, 0, new System.Windows.Forms.TreeNode[] {
+            treeNode21,
+            treeNode22,
+            treeNode23});
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // treeView1
@@ -67,81 +68,83 @@
             this.treeView1.ImageList = this.imageList1;
             this.treeView1.Location = new System.Drawing.Point(12, 12);
             this.treeView1.Name = "treeView1";
-            treeNode1.ImageIndex = 2;
-            treeNode1.Name = "노드4";
-            treeNode1.SelectedImageIndex = 2;
-            treeNode1.Text = "베토벤";
-            treeNode2.ImageIndex = 2;
-            treeNode2.Name = "노드5";
-            treeNode2.SelectedImageIndex = 2;
-            treeNode2.Text = "모차르트";
-            treeNode3.ImageIndex = 2;
-            treeNode3.Name = "노드6";
-            treeNode3.SelectedImageIndex = 2;
-            treeNode3.Text = "차이코프스키";
-            treeNode4.ImageIndex = 1;
-            treeNode4.Name = "Node0";
-            treeNode4.SelectedImageIndex = 0;
-            treeNode4.Text = "클래식";
-            treeNode5.ImageIndex = 2;
-            treeNode5.Name = "노드7";
-            treeNode5.SelectedImageIndex = 2;
-            treeNode5.Text = "노드7";
-            treeNode6.ImageIndex = 2;
-            treeNode6.Name = "노드8";
-            treeNode6.SelectedImageIndex = 2;
-            treeNode6.Text = "노드8";
-            treeNode7.ImageIndex = 2;
-            treeNode7.Name = "노드9";
-            treeNode7.SelectedImageIndex = 2;
-            treeNode7.Text = "노드9";
-            treeNode8.ImageIndex = 1;
-            treeNode8.Name = "Node1";
-            treeNode8.SelectedImageIndex = 0;
-            treeNode8.Text = "팝";
-            treeNode9.ImageIndex = 2;
-            treeNode9.Name = "노드10";
-            treeNode9.SelectedImageIndex = 2;
-            treeNode9.Text = "노드10";
-            treeNode10.ImageIndex = 2;
-            treeNode10.Name = "노드11";
-            treeNode10.SelectedImageIndex = 2;
-            treeNode10.Text = "노드11";
-            treeNode11.ImageIndex = 2;
-            treeNode11.Name = "노드12";
-            treeNode11.SelectedImageIndex = 2;
-            treeNode11.Text = "노드12";
-            treeNode12.ImageIndex = 1;
-            treeNode12.Name = "Node2";
-            treeNode12.SelectedImageIndex = 0;
-            treeNode12.Text = "K-pop";
+            treeNode13.ImageIndex = 2;
+            treeNode13.Name = "노드4";
+            treeNode13.SelectedImageIndex = 2;
+            treeNode13.Text = "베토벤";
+            treeNode14.ImageIndex = 2;
+            treeNode14.Name = "노드5";
+            treeNode14.SelectedImageIndex = 2;
+            treeNode14.Text = "모차르트";
+            treeNode15.ImageIndex = 2;
+            treeNode15.Name = "노드6";
+            treeNode15.SelectedImageIndex = 2;
+            treeNode15.Text = "차이코프스키";
+            treeNode16.ImageIndex = 1;
+            treeNode16.Name = "Node0";
+            treeNode16.SelectedImageIndex = 0;
+            treeNode16.Text = "클래식";
+            treeNode17.ImageIndex = 2;
+            treeNode17.Name = "노드7";
+            treeNode17.SelectedImageIndex = 2;
+            treeNode17.Text = "노드7";
+            treeNode18.ImageIndex = 2;
+            treeNode18.Name = "노드8";
+            treeNode18.SelectedImageIndex = 2;
+            treeNode18.Text = "노드8";
+            treeNode19.ImageIndex = 2;
+            treeNode19.Name = "노드9";
+            treeNode19.SelectedImageIndex = 2;
+            treeNode19.Text = "노드9";
+            treeNode20.ImageIndex = 1;
+            treeNode20.Name = "Node1";
+            treeNode20.SelectedImageIndex = 0;
+            treeNode20.Text = "팝";
+            treeNode21.ImageIndex = 2;
+            treeNode21.Name = "노드10";
+            treeNode21.SelectedImageIndex = 2;
+            treeNode21.Text = "노드10";
+            treeNode22.ImageIndex = 2;
+            treeNode22.Name = "노드11";
+            treeNode22.SelectedImageIndex = 2;
+            treeNode22.Text = "노드11";
+            treeNode23.ImageIndex = 2;
+            treeNode23.Name = "노드12";
+            treeNode23.SelectedImageIndex = 2;
+            treeNode23.Text = "노드12";
+            treeNode24.ImageIndex = 1;
+            treeNode24.Name = "Node2";
+            treeNode24.SelectedImageIndex = 0;
+            treeNode24.Text = "K-pop";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode4,
-            treeNode8,
-            treeNode12});
+            treeNode16,
+            treeNode20,
+            treeNode24});
             this.treeView1.SelectedImageIndex = 0;
-            this.treeView1.Size = new System.Drawing.Size(563, 289);
+            this.treeView1.Size = new System.Drawing.Size(563, 263);
             this.treeView1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.button1.Location = new System.Drawing.Point(201, 367);
+            this.button1.Location = new System.Drawing.Point(252, 312);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 30);
             this.button1.TabIndex = 1;
             this.button1.Text = "노드추가";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.button2.Location = new System.Drawing.Point(304, 367);
+            this.button2.Location = new System.Drawing.Point(346, 312);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(80, 30);
             this.button2.TabIndex = 1;
             this.button2.Text = "노드제거";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // imageList1
             // 
@@ -155,22 +158,35 @@
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(12, 307);
+            this.textBox1.Location = new System.Drawing.Point(9, 281);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(563, 25);
             this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button3.Location = new System.Drawing.Point(158, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 30);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "루트추가";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 450);
+            this.ClientSize = new System.Drawing.Size(584, 355);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.treeView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,6 +199,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WinFormsApp/WindowsFormsApp1/Form1.cs
+++ WinFormsApp/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,36 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            treeView1.ExpandAll();      // 모든 노드를 펼침
+            treeView1.CollapseAll();    // 모든 노드를 접음
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "" && treeView1.SelectedNode != null)
+            {
+                treeView1.SelectedNode.Nodes.Add(new TreeNode(textBox1.Text, 2, 2));
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+            else
+            {
+                MessageBox.Show("노드 추가할 이름을 넣고, 추가할 폴더를 선택하세요");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Remove(treeView1.SelectedNode);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Add(new TreeNode(textBox1.Text, 1, 0));
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
     }
 }
